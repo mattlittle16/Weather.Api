049465f65f633b490d20297410936c19d5e8a348
Matt Little <matt.little@coxautoinc.com>
2024-03-09 08:49:53 -0500

    migrations

---
+++ src/Infrastructure/Logger/DbLogger.cs
@@ -0,0 +1,100 @@
+using System.Reflection;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Core.Entities;
+using Core.Enums;
+using Infrastructure.Interfaces;
+using Microsoft.Extensions.Logging;
+using MySqlConnector;
+
+namespace Infrastructure.Logger;
+
+public class DbLogger : ILogger  
+{  
+    private readonly DbLoggerProvider _dbLoggerProvider;   
+    
+    public DbLogger(DbLoggerProvider dbLoggerProvider)  
+    {  
+        _dbLoggerProvider = dbLoggerProvider;   
+    }  
+
+    public IDisposable BeginScope<TState>(TState state)  
+    {  
+        return null;  
+    }  
+
+    public bool IsEnabled(LogLevel logLevel)  
+    {  
+        return logLevel != LogLevel.None;  
+    }  
+
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)  
+    {  
+        if (!IsEnabled(logLevel))  
+        {   
+            return;  
+        }  
+
+        var threadId = Thread.CurrentThread.ManagedThreadId; // Get the current thread ID to use in the log file.   
+
+        // Store record.  
+        using (var connection = new MySqlConnection(_dbLoggerProvider._ConString))  
+        {  
+            connection.Open();  
+
+            FieldInfo[] fieldsInfo = state.GetType().GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
+            FieldInfo props = fieldsInfo.FirstOrDefault(o => o.Name == "_values");
+            object[] values = (object[])props?.GetValue(state);
+
+            var logType = LogTypeEnum.General;
+            var logTypeId = string.Empty;
+            
+            var message = $"{JsonSerializer.Serialize(eventId)} || ";
+            if (exception != null)
+            {
+                message += JsonSerializer.Serialize(exception);
+            }
+            else 
+            {
+                message += JsonSerializer.Serialize(state);
+            }
+
+            try 
+            {
+                // Add to database.  
+                using (var command = new MySqlCommand())  
+                {  
+                    command.Connection = connection;  
+                    command.CommandType = System.Data.CommandType.Text;  
+                    command.CommandText = string.Format("select Id from LogType where Type = @Type");
+                    command.Parameters.Add(new MySqlParameter("@Type", logType));
+                    command.ExecuteNonQuery();
+                    using (var reader = command.ExecuteReader())
+                    {
+                        reader.Read();
+                        logTypeId = reader[0].ToString();
+                    }
+
+
+                    command.Connection = connection;  
+                    command.CommandType = System.Data.CommandType.Text;  
+                    command.CommandText = string.Format(@"INSERT INTO Log (Id, LogTypeId, Message, CreatedDate) 
+                    VALUES (@Id, @LogTypeId, @Message, @CreatedDate)");  
+
+                    command.Parameters.Add(new MySqlParameter("@Id", Guid.NewGuid()));
+                    command.Parameters.Add(new MySqlParameter("@LogTypeId", logTypeId));
+                    command.Parameters.Add(new MySqlParameter("@Message", message));
+                    command.Parameters.Add(new MySqlParameter("@CreatedDate", DateTimeOffset.UtcNow));
+
+                    command.ExecuteNonQuery();  
+                }  
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            connection.Close();  
+        }  
+    }  
+}  
---
+++ src/Infrastructure/MySql/Scripts/foreignkeychange.sql
@@ -0,0 +1,20 @@
+﻿START TRANSACTION;
+
+ALTER TABLE `Log` DROP FOREIGN KEY `FK_Log_LogType_Id`;
+
+CREATE INDEX `IX_Log_LogTypeId` ON `Log` (`LogTypeId`);
+
+ALTER TABLE `Log` ADD CONSTRAINT `FK_Log_LogType_LogTypeId` FOREIGN KEY (`LogTypeId`) REFERENCES `LogType` (`Id`) ON DELETE CASCADE;
+
+INSERT INTO `__EFMigrationsHistory` (`MigrationId`, `ProductVersion`)
+VALUES ('20240309134126_KeyChange', '8.0.2');
+
+COMMIT;
+
+START TRANSACTION;
+
+INSERT INTO `__EFMigrationsHistory` (`MigrationId`, `ProductVersion`)
+VALUES ('20240309134344_ForeignKeyChange', '8.0.2');
+
+COMMIT;
+
