3d4041d308e48f75b0d39b8f3afa707586d3f207
Matt Little <mituw16@gmail.com>
2024-12-03 11:07:21 -0800

    fixing unit tests

--- src/UnitTests/Controllers/GeocodeController.cs
+++ src/UnitTests/Controllers/GeocodeController.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using AutoFixture.Xunit2;
 using Core.Interfaces;
 using Core.Models;
@@ -5,7 +6,10 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Moq;
 using Api.Controllers;
+using Api.Validators;
 using AutoFixture;
+using Core.RequestModels;
+using FluentValidation;
 
 namespace UnitTests.Controllers;
 
@@ -18,17 +22,20 @@ public class GeocodeControllerTests
         [Frozen] Mock<IWeatherService> weatherServiceMock, 
         [Frozen] Mock<ILogger<GeocodeController>> loggerMock,
         string city, 
+        [MaxLength(2)]
         string state, 
+        [MaxLength(5)]
         string postalCode, 
         Geocode serviceResponse
     )
     {
         //Arrange 
+        var validator = new GeocodeRequestValidator();
         weatherServiceMock.Setup(x => x.GetGeocodeAsync(city, state, postalCode)).ReturnsAsync(serviceResponse);
-        var controller = new GeocodeController(loggerMock.Object, weatherServiceMock.Object);
+        var controller = new GeocodeController(loggerMock.Object, weatherServiceMock.Object, validator);
         
         //Act 
-        var response = await controller.Get(city, state, postalCode);
+        var response = await controller.Get(new GeocodeRequestModel { City = city, PostalCode = postalCode, State = state});
 
         //Assert
         var result = response;
